e35863634b6f5d7db20e45eccb971b02109136d1
Bathla1610 <stbathla@deloitte.com>
2026-04-18 11:01:02 +0530

    vercel blank page fixed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/Anaplan-Support-Portal/',
+  base: '/',
   plugins: [react()],
   server: {
     proxy: {
